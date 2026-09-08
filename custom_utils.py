21d6211ab1143254f3ae2b6854fde15aff6bd7c5
hanseo <hanseok631@gmail.com>
2026-09-08 10:42:48 +0900

    Sorting Images into respective trip ranges complete

---
+++ custom_utils.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+
+from PIL import ExifTags
+
+JSON_Date_Format = "%Y-%m-%d"
+Photo_DateTime_Format = "%Y:%m:%d %H:%M:%S"
+Photo_Time_Format = "%H:%M:%S"
+
+
+def extract_datetime(img):
+    exif_data = img.getexif()
+    if exif_data:
+        exif_ifd = exif_data.get_ifd(ExifTags.IFD.Exif)
+        date_time_original = exif_ifd.get(ExifTags.Base.DateTimeOriginal)
+        datetime_object = datetime.strptime(  # noqa: DTZ007
+            date_time_original, Photo_DateTime_Format
+        )
+        EXIF_exists = True
+    else:
+        datetime_object = 0
+        EXIF_exists = False
+
+    return datetime_object, EXIF_exists
+
+
+def extract_trip_data(trip):
+    start_date = datetime.strptime(trip["start"], JSON_Date_Format)  # noqa: DTZ007
+    end_date = datetime.strptime(  # noqa: DTZ007
+        trip["end"] + " 23:59:59", JSON_Date_Format + " %H:%M:%S"
+    )
+    return start_date, end_date
+
+
+def find_matching_location(Photo_DateTime, Trips):
+    for trip in Trips:
+        start, end = extract_trip_data(trip)
+        if start <= Photo_DateTime <= end:
+            matched_location = f"{trip['country']}: {trip['city']}"
+            return matched_location
--- main.py
+++ main.py
@@ -1,5 +1,32 @@
+import json
+from pathlib import Path
+
+from PIL import Image
+
+import custom_utils
+
+JSON_Date_Format = "%Y-%m-%d"
+Photo_Date_Format = "%Y:%m:%d"
+Photo_Time_Format = "%H:%M:%S"
+
+rename_log = {}
+
+
 def main():
-    print("Hello from p03!")
+    # Import JSON file
+    with open("Parameters/schedule.json", "r") as file:
+        Trips = json.load(file)
+
+    # Open images iteratively
+    for item in Path("Photos").iterdir():
+        if item.suffix != ".JPG":
+            continue
+        Photo_datetime, EXIF_exists = custom_utils.extract_datetime(Image.open(item))
+        if EXIF_exists == False:
+            continue
+            print(f"No EXIF in {item}")
+        matched_location = custom_utils.find_matching_location(Photo_datetime, Trips)
+        print(f"{item} >>> {matched_location}")
 
 
 if __name__ == "__main__":
